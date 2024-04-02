bf50775aaddccfe51be0ee13c6958d063811b690
Evelyn <lhnguye1@ualberta.ca>
2024-04-02 09:19:45 -0600

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+const App = () => {
+  // console log here to check if it is working
+  console.log('App.tsx loaded')
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello, World!
+    </h1>
+  )
+}
+
+export default App
