1fedc7c6488a8badf89b322d82ba6560bb90d04c
Evelyn <lhnguye1@ualberta.ca>
2024-04-03 09:35:14 -0600

    Fix: Centered the signup form on the screen

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,23 +1,23 @@
 import { Outlet, Navigate } from "react-router-dom";
 import Spline from "@splinetool/react-spline";
+
 const AuthLayout = () => {
   const isAuthenticated = false;
-
-  console.log('AuthLayout');
   return (
     <>
       {isAuthenticated ? 
       (<Navigate to="/" />) : 
       (
-        <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
-            <Outlet />
-          </section>
-          <div className="w-1/2">
+        <div className="flex">
+          <div className="h-screen flex-1 flex items-center justify-center">
+            <section>
+              <Outlet />
+            </section>
+          </div>
+          <div className="flex-1">
             <Spline scene="https://prod.spline.design/n-jcr-WS28CAsiIq/scene.splinecode" />
           </div>
-        </>
-
+        </div>
       )}
     </>
   )
