acaa09de8497aaa1f6ee486c9db71f098aa92c57
Evelyn <lhnguye1@ualberta.ca>
2024-04-03 12:43:09 -0600

    Init react-query

---
+++ src/lib/react-query/queriesAndMutations.tsx
@@ -0,0 +1,38 @@
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    useInfiniteQuery,
+  } from "@tanstack/react-query";
+  
+  import {
+    createUserAccount,
+    signInAccount,
+    getCurrentUser,
+    signOutAccount,
+  } from "@/lib/appwrite/api";
+  import { INewPost, INewUser, IUpdatePost, IUpdateUser } from "@/types";
+  
+  // ============================================================
+  // AUTH QUERIES
+  // ============================================================
+  
+  export const useCreateUserAccount = () => {
+    return useMutation({
+      mutationFn: (user: INewUser) => createUserAccount(user),
+    });
+  };
+  
+  export const useSignInAccount = () => {
+    return useMutation({
+      mutationFn: (user: { email: string; password: string }) =>
+        signInAccount(user),
+    });
+  };
+  
+  export const useSignOutAccount = () => {
+    return useMutation({
+      mutationFn: signOutAccount,
+    });
+  };
+  
