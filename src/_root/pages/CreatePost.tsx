133718af3b61dbb949a35d1888f812300697c0ef
Evelyn <lhnguye1@ualberta.ca>
2024-04-11 13:42:57 -0600

    feat: Initialize home page

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -5,7 +5,7 @@ const CreatePost = () => {
     <div className="flex flex-1">
       <div className='common-container'>
         <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-          <h2 className="h3-bold mdLh2-bold text-left w-full">Create Post</h2>
+          <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
         </div>
         <PostForm />
       </div>
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,20 @@
+import LazyLoader from "@/components/shared/LazyLoader";
+
 const Home = () => {
+  const isPostLoading = true;
+  const posts = null;
   return (
-    <div>
-      Root Layout
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h2-bold md:h3-bold text-left w-full">
+            Home
+            {isPostLoading && !posts ? 
+            (<LazyLoader />) : null}
+          </h2>
+        </div>
+      </div>
+      
     </div>
   )
 }
