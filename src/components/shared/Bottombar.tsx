85844c3632cf013d205e41f5074a0b9d11f8e3d0
Evelyn <lhnguye1@ualberta.ca>
2024-04-11 12:03:57 -0600

    fix: Update UI to improve bottom bar responsiveness on phone

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -156,11 +156,13 @@ const Bottombar = () => {
             </div>
           </Button>
           <Button className={liveStreamContainterButtonName}>
-            <div className={liveStreamButtonName}>
-              <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
-              </svg>
-            </div>
+            <Link to="/create-post">
+              <div className={liveStreamButtonName}>
+                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
+                </svg>
+              </div>
+            </Link>
           </Button>
       </section>
     </>
